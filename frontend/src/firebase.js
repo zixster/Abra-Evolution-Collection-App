0b1c52ec5d558e64f20ad31271e3290d84c814b4
John Stewart <littleyellowbuss@yahoo.com>
2025-11-27 05:59:49 -0600

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECTID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDERID",
-  appId: "YOUR_APPID"
+  apiKey: "AIzaSyCjo6Jgd9Xc4tS3MhiWLS8AsD-dK6jVkUo",
+  authDomain: "abra-evolution-line-collection.firebaseapp.com",
+  projectId: "abra-evolution-line-collection",
+  storageBucket: "abra-evolution-line-collection.firebasestorage.app",
+  messagingSenderId: "990255521226",
+  appId: "1:990255521226:web:710ffca8e8469235894333",
+  measurementId: "G-NC9L5E822R"
 };
 
 const app = initializeApp(firebaseConfig);
